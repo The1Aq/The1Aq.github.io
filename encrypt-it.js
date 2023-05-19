2fcb2c0a6588bb41c0ddf2fc2f95924f6e4869ae
The1Aq <27236375+The1Aq@users.noreply.github.com>
2023-05-19 01:43:40 -0700

    encrypt

--- encrypt-it.js
+++ encrypt-it.js
@@ -1,26 +1,76 @@
-/*
- * Starter file 
- */
 (function() {
   "use strict";
-
-  /**
-   * The starting point in our program, setting up a listener
-   * for the "load" event on the window, signalling the HTML DOM has been constructed
-   * on the page. When this event occurs, the attached function (init) will be called.
-   */
   window.addEventListener("load", init);
-
-  /**
-   * TODO: Write a function comment using JSDoc.
-   */
   function init() {
-    // Note: In this function, we usually want to set up our event handlers
-    // for UI elements on the page.
+    console.log("Window loaded!");
+    document.getElementById("encrypt-it").addEventListener('click',handleClick);
+    document.getElementById("normal-size").addEventListener("click", fontSize);
+		document.getElementById("bigger-size").addEventListener("click", fontSize);
+    document.getElementById("all-caps").addEventListener("change", upper);
+    document.getElementById("reset").addEventListener('click',reset);
+    
+  }
+  
+
+
+  function handleClick(){
+    var plainText = document.getElementById("input-text").value;
+    var ciphertext =shiftCipher(plainText);
+    document.getElementById("result").innerText = ciphertext;
+    console.log("button clicked");
+
+
+  }
+  
+  function fontSize(){
+    if (document.getElementById("normal-size").checked){
+			
+      document.getElementById("result").style.fontSize = "12pt";
+		
+    } else {
+			
+      document.getElementById("result").style.fontSize = "24pt";
+		
+    }
+	}
+
+    
+  
+  function upper() {
+    var text = document.getElementById("result")
+    if(document.getElementById("all-caps").checked){
+      
+      text.innerText.toUpperCase();
+    
+    }else{
+
+     text.innerText.toLowerCase();
+    }
+   
   }
 
-  // Add any other functions in this area (you should not implement your
-  // entire program in the init function, for similar reasons that
-  // you shouldn't write an entire Java program in the main method).
+  function reset(){
+    document.getElementById("all-caps").checked = false;
+    document.getElementById("result").innerText ="";
+    document.getElementById("input-text").value="";
+    document.getElementById("result").style.fontSize = "12pt";
+    document.getElementById("normal-size").checked = true;
+  }
 
-})();
+function shiftCipher(text) {
+  text = text.toLowerCase();
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] < 'a' || text[i] > 'z') {
+      result += text[i];
+    } else if (text[i] == 'z') {
+      result += 'a';
+    } else { 
+      let letter = text.charCodeAt(i);
+      let resultLetter = String.fromCharCode(letter + 1);
+      result += resultLetter;
+    }
+  }
+  return result;
+}
+}());
